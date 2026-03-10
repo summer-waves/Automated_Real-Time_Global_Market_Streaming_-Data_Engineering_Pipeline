2b9fcfc9775154383c1b3656104dacbee2f4f20b
Marco O. <163684231+summer-waves@users.noreply.github.com>
2026-03-09 21:35:45 -0500

    Update 02_create_landing_tables.sql

--- SQL Infrastructure & ETL/02_create_landing_tables.sql
+++ SQL Infrastructure & ETL/02_create_landing_tables.sql
@@ -1,13 +1,14 @@
--- 1. Use your project home
+-- 1. Use project home
 USE DATABASE MARKET_INSIGHTS_DB;
 USE SCHEMA RAW_DATA_SCHEMA;
 
--- 2. THE MOMENT OF TRUTH: Sniff the headers
--- This failed before because of the AWS /* issue. It will work now!
+-- 2. Sniff the headers
+-
 SELECT *
 FROM TABLE(
   INFER_SCHEMA(
     LOCATION=>'@market_data_stage/NASDAQ_Stock.csv',
     FILE_FORMAT=>'global_stock_csv'
   )
+
 );
